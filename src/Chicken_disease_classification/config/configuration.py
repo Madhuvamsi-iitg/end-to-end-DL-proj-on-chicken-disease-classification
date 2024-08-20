49eed6e113d9c5dcdee226e28b3d31bee5040d0a
Madhuvamsi <madhuvamsi135@gmail.com>
2024-08-20 17:24:10 +0530

    evaluation completed

--- src/Chicken_disease_classification/config/configuration.py
+++ src/Chicken_disease_classification/config/configuration.py
@@ -1,7 +1,8 @@
 from Chicken_disease_classification.constants import * 
 import os
+from pathlib import Path
 from Chicken_disease_classification.utils.common import read_yaml,create_directories
-from Chicken_disease_classification.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig)
+from Chicken_disease_classification.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig,TrainingConfig,EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -88,3 +89,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
